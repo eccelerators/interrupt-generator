a589530f464f695f5d62d7f0ad6dc65f65b71a77
Heinrich Diebel <heinrich.diebel@eccelerators.com>
2024-03-26 06:27:49 +0100

    eccelerators basic package not in separate lib

--- src/vhdl/InterruptGenerator.vhd
+++ src/vhdl/InterruptGenerator.vhd
@@ -31,9 +31,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library eccelerators;
-    use eccelerators.basic.all;
-
+use work.basic.all;
 use work.InterruptGeneratorIfcPackage.all;
 
 
